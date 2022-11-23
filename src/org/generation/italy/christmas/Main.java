b824a0f3e1a6a32d2c8101ae1e63abbe6008802e
DanCan30 <danielecan3008@gmail.com>
2022-11-23 14:55:24 +0100

    Comparator by vowels count added

--- src/org/generation/italy/christmas/Main.java
+++ src/org/generation/italy/christmas/Main.java
@@ -1,10 +1,41 @@
 package org.generation.italy.christmas;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+	
+	public static class StringComparator implements Comparator<String> {
+
+		@Override
+		public int compare(String o1, String o2) {
+
+			int o1VowelCount = 0;
+			int o2VowelCount = 0;
+			
+			for(int i = 0; i < o1.length(); i++) {
+				
+				if(o1.charAt(i) == 'a' || o1.charAt(i) == 'e' || o1.charAt(i) == 'i' ||
+						o1.charAt(i) == 'o' || o1.charAt(i) == 'u') {
+					o1VowelCount ++;
+				}
+			}
+			
+			for(int i = 0; i < o2.length(); i++) {
+				
+				if(o2.charAt(i) == 'a' || o2.charAt(i) == 'e' || o2.charAt(i) == 'i' ||
+						o2.charAt(i) == 'o' || o2.charAt(i) == 'u') {
+					o2VowelCount ++;
+				}
+			}
+			
+			return o2VowelCount - o1VowelCount;
+		}
+		
+		
+	}
 
 	public static void main(String[] args) {
 		
@@ -28,8 +59,8 @@ public class Main {
 			
 			if(userChoice.equals("n")) {
 				done = true;
-				wishlist.sort(null);
-				System.out.println("Your final wishlist is: ");
+				wishlist.sort(new StringComparator());
+				System.out.println("Your final wishlist ordered by vowels count is: ");
 				
 				for(String element : wishlist) {
 					System.out.println("-" + element);
---
+++ src/org/generation/italy/christmas/SetAndMap.java
@@ -0,0 +1,8 @@
+package org.generation.italy.christmas;
+
+public class SetAndMap {
+
+	public static void main(String[] args) {
+		
+	}
+}
